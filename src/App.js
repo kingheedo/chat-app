33f719031e78ec6a60dab26776e7a77c1d93abab
heedo-wang <dhkdgmleh@gmail.com>
2021-01-05 04:45:16 +0900

    Redux 스토어에 로그인 유저 정보 저장

--- src/App.js
+++ src/App.js
@@ -9,27 +9,43 @@ import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import firebase from './firebase';
 
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  setUser
+} from './redux/actions/user_action'
+
 function App() {
   let history = useHistory();
-
+  let dispatch = useDispatch();
+  const isLoading = useSelector(state=> state.user.isLoading)
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user=>{
       console.log('user',user)
       //로그인이 된 상태
       if(user){
         history.push("/")
+        dispatch(setUser(user))
       }else{
         history.push("/login")
       }
     })
   }, [])
-  return (
+  if(isLoading){
+    return(
+      <div>
+        ...loading
+      </div>
+    )
+  }else{
+    return (
       <Switch>
         <Route exact path="/" component={ChatPage}/>
         <Route exact path="/login" component={LoginPage}/>
         <Route exact path="/register" component={RegisterPage}/>
       </Switch>
   )
+  }
+  
 }
 
 export default App;
