2c2e763bdf1a1b74c5f44ff6159bdb6d234d4475
heedo-wang <dhkdgmleh@gmail.com>
2021-01-05 04:18:44 +0900

    인증된 이후의 페이지 이동 & useHistory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from "react";
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  useHistory
 } from "react-router-dom";
 import ChatPage from './components/ChatPage/ChatPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import firebase from './firebase';
+
 function App() {
+  let history = useHistory();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user=>{
+      console.log('user',user)
+      //로그인이 된 상태
+      if(user){
+        history.push("/")
+      }else{
+        history.push("/login")
+      }
+    })
+  }, [])
   return (
-    <Router>
       <Switch>
         <Route exact path="/" component={ChatPage}/>
         <Route exact path="/login" component={LoginPage}/>
         <Route exact path="/register" component={RegisterPage}/>
       </Switch>
-  </Router>
   )
 }
 
