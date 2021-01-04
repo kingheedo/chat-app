cc94f0df729f29d66ab680b4fa1cc1ea51b4023e
heedo-wang <dhkdgmleh@gmail.com>
2021-01-05 03:56:01 +0900

    로그인 페이지 만들기

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={ChatPage}/>
-        <Route exact path="/loign" component={LoginPage}/>
+        <Route exact path="/login" component={LoginPage}/>
         <Route exact path="/register" component={RegisterPage}/>
       </Switch>
   </Router>
