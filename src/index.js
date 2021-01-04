e3e297030a195058ed8d80c8894afbb62a4da3d3
heedo-wang <dhkdgmleh@gmail.com>
2021-01-04 20:28:41 +0900

    기본구조 만들기

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './redux/reducers'
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore) 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__&&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
