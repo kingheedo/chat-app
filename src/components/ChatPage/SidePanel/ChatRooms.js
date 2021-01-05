4cae4c99a69f03b28257b95c5f44e8fd6330e25e
heedo-wang <dhkdgmleh@gmail.com>
2021-01-05 22:42:51 +0900

    채팅룸 UI,ChatRoom 생성하기 및 Firebase에서 데이터 실시간으로 받기

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {FaRegSmileWink} from 'react-icons/fa'
+import {FaPlus} from 'react-icons/fa'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
+import {connect} from 'react-redux';
+import firebase from '../../../firebase'
+export class ChatRooms extends Component {
+    state={
+        show:false,
+        name:"",
+        description:"",
+        chatRoomsRef:firebase.database().ref("chatRooms")
+    }
 
-function ChatRooms() {
-    return (
-        <div>
-            ChatRooms
-        </div>
-    )
+
+    handleClose = () => this.setState({show:false});
+    handleShow = () => this.setState({show:true});
+    handleSubmit = (e) =>{
+        e.preventDefault();
+
+        const {name,description} = this.state;
+
+        if(this.isFormValid(name,description)){
+            this.addChatRoom();
+        }
+    }
+    addChatRoom = async() =>{
+        const key = this.state.chatRoomsRef.push().key;
+        const {name,description} = this.state;
+        const {user} = this.props
+        const newChatRoom ={
+            id:key,
+            name:name,
+            description: description,
+            createdBy:{
+                name:user.displayName,
+                image:user.photoURL
+            }
+        }
+        try{
+            await this.state.chatRoomsRef.child(key).update(newChatRoom)
+            this.setState({
+                name:"",
+                description:"",
+                show:false
+            })
+        }catch(error){
+            alert(error)
+        }
+    }
+    isFormValid = (name,description) =>
+    name && description;
+    render() {
+        return (
+            <div>
+                
+                <div style={{
+                    position:'relative', width:'100%',
+                    display:'flex', alignItems:'center'
+                }}>
+                        <FaRegSmileWink style={{marginRight:3}}/>
+                        CHAT ROOMS{" "} (1)
+
+                        <FaPlus style={{
+                            position:'absolute',
+                            right:0, cursor:'pointer'
+                        }}
+                        onClick={this.handleShow}
+                        />
+
+
+                </div>
+                {/* ADD CHAT ROOM MODAL */}
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Create a chat room</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        
+                        
+                        <Form onSubmit ={this.handleSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>방 이름</Form.Label>
+                            <Form.Control 
+                            onChange={(e)=>{this.setState({name:e.target.value})}}
+                            type="text" placeholder="Enter a chat room name" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>방 설명</Form.Label>
+                            <Form.Control
+                            onChange={(e)=>{this.setState({description:e.target.value})}}
+                            type="text" placeholder="Enter a chat room description" />
+                        </Form.Group>
+                        </Form>                        
+                        
+                        
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={this.handleSubmit}>
+                        Create
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
 }
-
-export default ChatRooms
+const mapStateToProps = state =>{
+    return {
+        user: state.user.currentUser
+    }
+}
+export default connect(mapStateToProps) (ChatRooms)
