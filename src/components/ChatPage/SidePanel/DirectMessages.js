632bfb173552ee65277bf71111acb07ef79bdcca
heedo-wang <dhkdgmleh@gmail.com>
2021-01-08 17:14:53 +0900

    Diret Message 기본 UI 및 User 목록 가져오기

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {FaRegSmile} from 'react-icons/fa'
+import firebase from '../../../firebase'
+import {connect} from 'react-redux'
+export class DirectMessages extends Component {
+    state ={
+        usersRef: firebase.database().ref("users")
+    }
+    componentDidMount(){
+        if(this.props.user){
+        this.addUsersListeners(this.props.user.uid)
+            
+        }
+    }
+    addUsersListeners = (currentUserId) =>{
+        const {usersRef}  =this.state;
+        let usersArray = [];
+        usersRef.on("child_added", DataSnapshot=>{
+            if(currentUserId !== DataSnapshot.key){
+                console.log('datasnapshot.val()', DataSnapshot.val())
+                let user = DataSnapshot.val()
+                user["uid"] = DataSnapshot.key
+                user["status"] = "offline";
+                usersArray.push(user)
+                this.setState({users:usersArray})
+            }
+        })
+    }
+    renderDirectMessages = () =>{
 
-function DirectMessages() {
-    return (
-        <div>
-            DirectMessages
-        </div>
-    )
+    }
+    render() {
+        console.log('users',this.state.users)
+        return (
+            <div>
+                <span style={{display:'flex', alignItems:'center'}}>
+                <FaRegSmile style={{marginRight :3}}/> DIRECT MESSAGES(1)
+                </span>
+                <ul>
+                    {this.renderDirectMessages()}
+                </ul>
+            </div>
+        )
+    }
 }
-
-export default DirectMessages
+const mapStatetoProps = state =>{
+    return{
+        user:state.user.currentUser
+    }
+}
+export default connect(mapStatetoProps) (DirectMessages)
